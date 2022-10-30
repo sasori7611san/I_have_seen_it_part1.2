dddc34531946d126a5061fb8f04e14e247286025
sasori7611san <107899079+sasori7611san@users.noreply.github.com>
2022-10-30 17:17:48 +0900

    修正

---
+++ vue-project/src/components/ChoiceColor.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="choice">
+        <button id="buttonRed" v-on:click="choiceColor(2);"></button>
+        <button id="buttonGreen" v-on:click="choiceColor(3);"></button>
+        <button id="buttonWhite" v-on:click="choiceColor(4);"></button>
+        <button id="buttonBlue" v-on:click="choiceColor(5);"></button><br />
+        <button id="buttonYellow" v-on:click="choiceColor(1);"></button>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
